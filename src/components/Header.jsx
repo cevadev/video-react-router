36f31a8173912f134b34e6c66f981413086c033c
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-10-19 22:49:46 -0300

    Curso react reux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
+import { object } from 'prop-types';
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
+function Header(props){
+  //objeto user
+  const { user } = props  ;
 
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+  //validamos si tenemos o no un user
+  const hasUser = Object.keys(user).length > 0;
+
+  return(
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {
+            hasUser ? <img src={gravatar(user.email)} alt={user.email} /> : <img src={userIcon} alt="" />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li><a href="/">Cuenta</a></li>
+          <li><Link to="/login">Iniciar Sesión</Link></li>
+        </ul>
       </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><Link to="/login">Iniciar Sesión</Link></li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+} 
 
-export default Header;
+//export default Header;
+
+/**
+ * Vamos a traer del estado el elemento que se encarga de los usuarios
+ */
+const mapStateToProps = state =>{
+  return{
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(Header)
